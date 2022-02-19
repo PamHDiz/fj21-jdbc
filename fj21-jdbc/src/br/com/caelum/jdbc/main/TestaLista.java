a1e887b3edc4724e451a5b8ce95a8137dfb07754
PamHDiz <pamhdiz@gmail.com>
2022-02-19 16:52:08 -0300

    Métodos para fazer pesquisa no BD e devolver uma List

---
+++ fj21-jdbc/src/br/com/caelum/jdbc/main/TestaLista.java
@@ -0,0 +1,26 @@
+package br.com.caelum.jdbc.main;
+
+import java.util.List;
+
+import br.com.caelum.jdbc.dao.ContatoDao;
+import br.com.caelum.jdbc.modelo.Contato;
+
+public class TestaLista {
+
+	public static void main(String[] args) {
+		
+		ContatoDao dao = new ContatoDao();
+		
+		List<Contato> listaContatos = dao.getLista();
+		
+		for(Contato contato : listaContatos) {
+			System.out.println("Nome: " + contato.getNome());
+			System.out.println("Email: " + contato.getEmail());
+			System.out.println("Endereço: " + contato.getEndereco());
+			System.out.println("Data de Nascimento: " + 
+					contato.getDataNascimento().getTime() + "\n");
+		}
+
+	}
+
+}
